42fc189f500e554b4a50fa36df5ac50df28d6c66
Maco Cortes <maco.techdepot@gmail.com>
2017-06-17 22:20:59 +0800

    GUI for login, vote page, and success page

---
+++ application/views/vote/vote_success.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * Copyright (c)2016, Jenmar "Maco" Cortes
+ * Copyright TechDepot PH
+ * All Rights Reserved
+ * 
+ * This license is a legal agreement between you and the Maco Cortes
+ * for the use of ALUMNI INFORMATION SYSTEM referred to as the "Software"
+ * By obtaining the Software you agree to comply with the terms and conditions of this license.
+ *
+ * PERMITTED USE
+ * With approval from Maco Cortes, You are permitted to use the program for educational purposes only.
+ * 
+ * MODIFICATION AND REDISTRIBUTION 
+ * Unless with written approval obtained from Maco Cortes, 
+ * You are NOT allowed to modify, copy, redistribute, and sell the Software.
+ *
+ * For any concerns, you may reach Maco Cortes via:
+ * maco.techdepot@gmail.com
+ * facebook.com/Maaacoooo
+ * maco@techdepot-ph.com
+ * TechDepot-PH.com
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-tap-highlight" content="no">
+  <title><?=$title?> &middot; <?=$site_title?></title>
+
+  <!-- Favicons-->
+  <link rel="icon" href="<?=base_url('assets/images/favicon/sti.png')?>" sizes="32x32">
+  <!-- Favicons-->
+
+  <link href="<?=base_url('assets/css/page.css')?>" type="text/css" rel="stylesheet" media="screen,projection">
+
+  <!-- CORE CSS-->
+
+  <link href="<?=base_url('assets/css/materialize.css')?>" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?=base_url('assets/css/style.css')?>" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?=base_url('assets/css/page-center.css')?>" type="text/css" rel="stylesheet" media="screen,projection">
+
+  <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+  <link href="<?=base_url('assets/css/prism.css')?>" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?=base_url('assets/js/plugins/perfect-scrollbar/perfect-scrollbar.css')?>" type="text/css" rel="stylesheet" media="screen,projection">
+  
+</head>
+
+<body class="indigo darken-2">
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+      <div id="loader"></div>        
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+  </div>
+  <!-- End Page Loading -->
+
+
+    <div class="col s12 m8 l6 z-depth-4 card-panel login-border vote-container">
+        <div class="row">
+          <div class="input-field col s12 center">
+            <img src="<?=base_url()?>assets/images/sti_header.png" alt="" class="responsive-img valign">
+            <p class="center login-form-text"><?=$site_title?></p>
+          </div>
+        </div>
+
+        <h1 class="center col s12">Your vote is Submitted!</h1>
+        <p class="center">Thank you for voting! To check the results, please visit the <a href="<?=base_url('vote/results')?>">Result Page</a>.</p><!-- /.center -->
+        <p class="center">Are you happy with the new Voting System? Rate us a Five Star!</p><!-- /.center -->
+ 
+      <?php $this->load->view('inc/copy_footer');?>
+    </div>
+
+
+    <!-- ================================================
+    Scripts
+    ================================================ -->
+
+    <!-- jQuery Library -->
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery-1.11.2.min.js')?>"></script>
+    <!--materialize js-->
+    <script type="text/javascript" src="<?=base_url('assets/js/materialize.js')?>"></script>
+    <!--prism-->
+    <script type="text/javascript" src="<?=base_url('assets/js/prism.js')?>"></script>
+    <!--scrollbar-->
+    <script type="text/javascript" src="<?=base_url('assets/js/plugins/perfect-scrollbar/perfect-scrollbar.min.js')?>"></script>
+
+    <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+    <script type="text/javascript" src="<?=base_url('assets/js/plugins.js')?>"></script>
+
+</body>
+</html>
