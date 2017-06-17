3504e66053234ed765e6d930de79d2610286faab
Maco Cortes <maco.techdepot@gmail.com>
2017-06-17 22:17:25 +0800

    Updated footer info

---
+++ application/views/inc/copy_footer.php
@@ -0,0 +1,8 @@
+<div class="copyright-footer grey-text">
+	<p>
+		<small>
+			Developed by <a href="#">congtibsh</a>
+			&copy; STI Dipolog 2017
+		</small>
+	</p>
+</div><!-- /.copyright-footer -->
