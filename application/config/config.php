1a17c77fbd3d002648f378a80392fb1d6ccaa929
Maco Cortes <maco.techdepot@gmail.com>
2017-06-18 10:24:07 +0800

    Changed Session Driver to 'database'

--- application/config/config.php
+++ application/config/config.php
@@ -330,7 +330,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'sad;lkasdmaskdlasdasdasdmaklsdaksd';
+$config['encryption_key'] = 'sadlkasdmassd';
 
 /*
 |--------------------------------------------------------------------------
@@ -383,10 +383,10 @@ $config['encryption_key'] = 'sad;lkasdmaskdlasdasdasdmaklsdaksd';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'sti_voting_sess';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
