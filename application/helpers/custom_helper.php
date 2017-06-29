539c3b1a170a8d04c05d3fb401412355918b93fd
Maco Cortes <maco.techdepot@gmail.com>
2017-06-29 22:23:09 +0800

    custom helper for cleancrypt

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -7,6 +7,19 @@
         return md5($salt.$str.$pepper);
     }
 
+    /**
+     * This provides an encrypted and/or unreadable data.
+     * @param  [type] $str [description]
+     * @return [type]      [description]
+     */
+    function cleancrypt($str) {
+
+    	$crypt_str 	= crypt(md5($str), 'TrRz'); //encodes the string
+    	$new_str 	= substr($crypt_str, 0, 6); //limits the string
+
+    	return $new_str;
+    }
+
 
   
 
