be95d71e7ecb492ba3c3b7354837db2dfc7da9d1
Maco Cortes <maco.techdepot@gmail.com>
2017-07-12 00:16:09 +0800

    create and paginate of user done

---
+++ application/controllers/sys/Users.php
@@ -0,0 +1,204 @@
+<?php
+
+/*
+ * Copyright (c)2017, Jenmar "Maco" Cortes
+ * Copyright TechDepot PH
+ * All Rights Reserved
+ * 
+ * This license is a legal agreement between you and the Maco Cortes
+ * for the use of STI Online Voting Systen referred to as the "Software"
+ * By obtaining the Software you agree to comply with the terms and conditions of this license.
+ *
+ * PERMITTED USE
+ * With approval from Maco Cortes, You are permitted to use the program for educational purposes only.
+ * 
+ * MODIFICATION AND REDISTRIBUTION 
+ * Unless with written approval obtained from Maco Cortes, 
+ * You are NOT allowed to modify, copy, redistribute, and sell the Software.
+ *
+ * For any concerns, you may reach Maco Cortes via:
+ * maco.techdepot@gmail.com
+ * facebook.com/Maaacoooo
+ * maco@techdepot-ph.com
+ * TechDepot-PH.com
+ */
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+
+	public function __construct()	{
+		parent::__construct();		
+       $this->load->model('user_model');
+       $this->load->model('candidates_model');
+	}	
+
+
+
+	public function index()		{
+
+		$userdata = $this->session->userdata('admin_logged_in'); //it's pretty clear it's a userdata
+
+		if($userdata)	{
+
+			$data['title'] 		= 'Users';
+			$data['site_title'] = APP_NAME;
+			$data['user'] 		= $this->user_model->userdetails($userdata['username']); //fetches users record
+
+
+			//Page Data 
+			$data['usertypes']		= $this->user_model->usertypes();
+
+			//Paginated data - Candidate Names				            
+	   		$config['num_links'] = 5;
+			$config['base_url'] = base_url('/sys/users/index/');
+			$config["total_rows"] = $this->user_model->count_users();
+			$config['per_page'] = 10;				
+			$this->load->config('pagination'); //LOAD PAGINATION CONFIG
+
+			$this->pagination->initialize($config);
+		    if($this->uri->segment(4)){
+		       $page = ($this->uri->segment(4)) ;
+		  	}	else 	{
+		       $page = 1;		               
+		    }
+
+		    $data["results"] = $this->user_model->fetch_users($config["per_page"], $page);
+		    $str_links = $this->pagination->create_links();
+		    $data["links"] = explode('&nbsp;',$str_links );
+
+		    //ITEM NUMBERING
+		    $data['per_page'] = $config['per_page'];
+		    $data['page'] = $page;
+
+		    //GET TOTAL RESULT
+		    $data['total_result'] = $config["total_rows"];
+		    //END PAGINATION		
+		
+			//Form Validation for candidate
+			$this->form_validation->set_rules('name', 'Name', 'trim|required'); 
+			$this->form_validation->set_rules('username', 'Username', 'trim|required|is_unique[users.username]|alpha_dash'); 
+			$this->form_validation->set_rules('usertype', 'Usertype', 'trim|required'); 
+			
+			
+
+			if($this->form_validation->run() == FALSE)	{
+				$this->load->view('admin/voting/user/list', $data);
+			} else {	
+		
+				//Proceed saving candidate				
+				if($this->user_model->create_user()) {			
+				
+					$this->session->set_flashdata('success', 'Succes! User registered!');
+					redirect($_SERVER['HTTP_REFERER'], 'refresh');
+				} else {
+					//failure
+					$this->session->set_flashdata('error', 'Oops! Error occured!');
+					redirect($_SERVER['HTTP_REFERER'], 'refresh');
+				}			
+				
+			}
+
+		} else {
+
+			$this->session->set_flashdata('error', 'You need to login!');
+			redirect('sys/dashboard/login', 'refresh');
+		}
+
+	}
+
+	public function update($id)		{
+
+		$userdata = $this->session->userdata('admin_logged_in'); //it's pretty clear it's a userdata
+
+		if($userdata)	{
+
+			$data['title'] = 'Update Candidate';
+			$data['site_title'] = APP_NAME;
+			$data['user'] = $this->user_model->userdetails($userdata['username']); //fetches users record
+
+			//Page Data 
+			$data['years']		= $this->candidates_model->years();
+			$data['courses']	= $this->candidates_model->courses();
+			$data['party']		= $this->candidates_model->party();
+			$data['positions']	= $this->candidates_model->positions();
+
+			$data['info']		= $this->candidates_model->read_candidate($id);
+
+			//Validate if record exist
+			 //IF NO ID OR NO RESULT, REDIRECT
+				if(!$id OR !$data['info']) {
+					redirect('sys/candidates', 'refresh');
+			}	
+
+			//Form Validation for candidate
+			$this->form_validation->set_rules('name', 'Name', 'trim|required'); 
+			$this->form_validation->set_rules('party', 'Partylist', 'trim|required'); 
+			$this->form_validation->set_rules('course', 'Courses', 'trim|required'); 
+			$this->form_validation->set_rules('year', 'Year', 'trim|required'); 
+			$this->form_validation->set_rules('id', 'ID', 'trim|required'); 
+		
+
+			if($this->form_validation->run() == FALSE)	{
+				$this->load->view('admin/voting/candidate/update', $data);
+			} else {			
+
+				//Proceed saving candidate				
+				$key_id = $this->encryption->decrypt($this->input->post('id')); //ID of the row
+				if($this->candidates_model->update_candidate($key_id)) {			
+				
+					$this->session->set_flashdata('success', 'Succes! Candidate Updated!');
+					redirect($_SERVER['HTTP_REFERER'], 'refresh');
+				} else {
+					//failure
+					$this->session->set_flashdata('error', 'Oops! Error occured!');
+					redirect($_SERVER['HTTP_REFERER'], 'refresh');
+				}			
+				
+			}			
+
+		} else {
+
+			$this->session->set_flashdata('error', 'You need to login!');
+			redirect('sys/dashboard/login', 'refresh');
+		}
+
+	}
+
+
+	public function delete()		{
+
+		$userdata = $this->session->userdata('admin_logged_in'); //it's pretty clear it's a userdata
+
+		if($userdata)	{
+
+			//FORM VALIDATION
+			$this->form_validation->set_rules('id', 'ID', 'trim|required');   
+		 
+		   if($this->form_validation->run() == FALSE)	{
+
+				$this->session->set_flashdata('error', 'An Error has Occured!');
+				redirect($_SERVER['HTTP_REFERER'], 'refresh');
+
+			} else {
+
+				$key_id = $this->encryption->decrypt($this->input->post('id')); //ID of the row				
+
+				if($this->candidates_model->delete_candidate($key_id)) {
+					$this->session->set_flashdata('success', 'Deleted!');
+					redirect($_SERVER['HTTP_REFERER'], 'refresh');
+				}
+			}
+
+		} else {
+
+			$this->session->set_flashdata('error', 'You need to login!');
+			redirect('sys/dashboard/login', 'refresh');
+		}
+
+	}
+
+
+
+}
---
+++ application/views/admin/voting/user/list.php
@@ -0,0 +1,237 @@
+<?php
+/*
+ * Copyright (c)2017, Jenmar "Maco" Cortes
+ * Copyright TechDepot PH
+ * All Rights Reserved
+ * 
+ * This license is a legal agreement between you and the Maco Cortes
+ * for the use of STI Online Voting Systen referred to as the "Software"
+ * By obtaining the Software you agree to comply with the terms and conditions of this license.
+ *
+ * PERMITTED USE
+ * With approval from Maco Cortes, You are permitted to use the program for educational purposes only.
+ * 
+ * MODIFICATION AND REDISTRIBUTION 
+ * Unless with written approval obtained from Maco Cortes, 
+ * You are NOT allowed to modify, copy, redistribute, and sell the Software.
+ *
+ * For any concerns, you may reach Maco Cortes via:
+ * maco.techdepot@gmail.com
+ * facebook.com/Maaacoooo
+ * maco@techdepot-ph.com
+ * TechDepot-PH.com
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
+    <title><?=$title?> &middot; <?=$site_title?></title>
+
+    <?php $this->load->view('inc/css'); ?>
+
+
+</head>
+
+<body>
+    
+    <?php $this->load->view('inc/header'); ?>
+
+    <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+
+  <!-- START MAIN -->
+  <div id="main">
+    <!-- START WRAPPER -->
+    <div class="wrapper">
+
+      <?php $this->load->view('inc/left_nav'); ?>
+
+      <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+      <!-- START CONTENT -->
+      <section id="content">
+        
+        <!--breadcrumbs start-->
+        <div id="breadcrumbs-wrapper" class=" grey lighten-3">
+          <div class="container">
+            <div class="row">
+              <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title"><?=$title?></h5>
+                <ol class="breadcrumb">
+                    <li><a href="#">Administrative Options</a></li>         
+                    <li class="active"><?=$title?></li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--breadcrumbs end-->
+        
+
+        <!--start container-->
+        <div class="container">
+          <div class="section">
+            <div class="row">
+              <div class="col s12">
+                <?php
+                //ERROR ACTION                          
+                  if($this->session->flashdata('error')) { ?>
+                    <div class="card-panel deep-orange darken-3">
+                        <span class="white-text"><i class="mdi-alert-warning tiny"></i> <?php echo $this->session->flashdata('error'); ?></span>
+                    </div>
+              <?php } ?> 
+              <?php
+                //SUCCESS ACTION                          
+                  if($this->session->flashdata('success')) { ?>
+                    <div class="card-panel green">
+                        <span class="white-text"><i class="mdi-action-done tiny"></i> <?php echo $this->session->flashdata('success'); ?></span>
+                    </div>
+              <?php } ?>             
+              <?php
+                //FORM VALIDATION ERROR
+                    $this->form_validation->set_error_delimiters('<p><i class="mdi-alert-warning tiny"></i> ', '</p>');
+                      if(validation_errors()) { ?>
+                    <div class="card-panel yellow amber">
+                        <span class="white-text"> <?php echo validation_errors(); ?></span>
+                    </div>
+              <?php } ?> 
+              </div>
+            </div>
+            
+          <div class="section">
+             <div class="row">
+               <div class="col s12 l7">
+                 <table class="striped bordered highlight">
+                  <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th></th>
+                    </tr>
+                  </thead>
+
+                  <tbody>                    
+                    <?php if($results):
+                      foreach($results as $row): ?>
+                    <tr>
+                      <td>
+                        <a href="<?=base_url('sys/users/update/' . $row['username'])?>">
+                        <?php if(filexist($row['img']) && $row['img']): ?>
+                          <img src="<?=base_url('uploads/'.$row['img'])?>" alt="" class="circle responsive-img valign candidate-img">
+                        <?php else: ?>
+                          <img src="<?=base_url('assets/images/no_image.gif')?>" alt="" class="circle responsive-img valign candidate-img">
+                        <?php endif; ?>
+                        </a>
+                      </td>
+                      <td><a href="<?=base_url('sys/users/update/' . $row['username'])?>"><?=$row['name']?></a></td>
+                      <td><?=$row['username']?></td>                 
+                      <td><?=$row['usertype']?></td>                 
+                    </tr> 
+                    <?php endforeach; 
+                      endif; ?>            
+                  </tbody>
+                </table>
+                <div class="right">
+                    <?php foreach ($links as $link) { echo $link; } ?>
+                </div>
+               </div><!-- /.col s12 l7 -->
+               <div class="col s12 l5">                
+                 <div class="card-panel">
+                   <div class="card-content">
+                     <h6 class="strong">Register User</h6><!-- /.strong -->
+                       <div class="card purple darken-4 white-text">
+                         <div class="card-content">
+                           <p><i class="mdi-action-info-outline tiny"></i> Every new user that is registered, a default password is set. <br/>
+                           The default password is <strong class="amber-text strong">STIDipolog</strong>.</p> <br />
+                           <p>Please advise your New User to change his password after logging in.</p>
+                         </div><!-- /.card-content -->
+                       </div><!-- /.card purple darken-4 white-text -->
+                     <?=form_open_multipart('sys/users')?>
+                       <div class="row">
+                         <div class="input-field col s12">
+                            <input id="username" name="username" type="text" class="validate" value="<?=set_value('username')?>" required>
+                            <label for="username">Username</label>
+                         </div>                         
+                       </div><!-- /.row -->
+                       <div class="row">
+                         <div class="input-field col s12">
+                            <input id="name" name="name" type="text" class="validate" value="<?=set_value('name')?>" required>
+                            <label for="name">Full Name</label>
+                         </div>                         
+                       </div><!-- /.row -->
+                       <div class="row">
+                         <div class="input-field col s12">
+                           <div class="file-field input-field">
+                            <div class="btn">
+                              <span>IMG</span>
+                              <input type="file" name="img">
+                            </div>
+                            <div class="file-path-wrapper">
+                              <input class="file-path validate" type="text">
+                            </div>
+                          </div>
+                         </div><!-- /.input-field col s12 l3 -->
+                       </div><!-- /.row -->
+                       <div class="row">
+                         <div class="input-field col s12">                        
+                            <div class="select-wrapper">  
+                              <select class="browser-default" name="usertype" required>
+                                  <option value="" disabled="" selected="">Select Usertype...</option>
+                                  <?php 
+                                    if($usertypes):
+                                    foreach($usertypes as $usr):
+                                  ?>
+                                  <option value="<?=$usr['title']?>"><?=$usr['title']?></option>
+                                  <?php
+                                    endforeach;
+                                    endif;
+                                  ?>
+                              </select>
+                            </div><!-- /.select-wrapper -->
+                            <label>Usertype</label>
+                          </div><!-- /.input-field col s12 -->
+                       </div><!-- /.row -->         
+                       <div class="row">
+                          <div class="input-field col s12">
+                              <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Submit
+                                <i class="mdi-content-send right"></i>
+                              </button>
+                           </div>
+                       </div><!-- /.row -->
+
+                       <input type="hidden" name="key" value="<?=$this->encryption->encrypt('candidate')?>" />
+                     <?=form_close()?>
+                   </div><!-- /.card-content -->
+                 </div><!-- /.card-panel -->       
+
+               </div><!-- /.col s12 l5 -->
+             </div><!-- /.row -->
+           </div><!-- /.section --> 
+         
+          </div>
+        </div>
+        <!--end container-->
+      </section>
+      <!-- END CONTENT -->
+
+    </div>
+    <!-- END WRAPPER -->
+
+  </div>
+  <!-- END MAIN -->
+
+
+
+     <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+    <?php $this->load->view('inc/footer'); ?>
+
+    <?php $this->load->view('inc/js'); ?>
+   
+</body>
+</html>
