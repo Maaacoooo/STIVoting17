9940e70e8d46cd356ab1ab5d1bde7df1ded5188b
Maco Cortes <maco.techdepot@gmail.com>
2017-07-13 16:44:40 +0800

    Removed stupid copyright stupid shit

--- application/views/inc/css.php
+++ application/views/inc/css.php
@@ -1,27 +1,3 @@
-<?php
-/*
- * Copyright (c)2017, Jenmar "Maco" Cortes
- * Copyright TechDepot PH
- * All Rights Reserved
- * 
- * This license is a legal agreement between you and the Maco Cortes
- * for the use of STI Online Voting Systen referred to as the "Software"
- * By obtaining the Software you agree to comply with the terms and conditions of this license.
- *
- * PERMITTED USE
- * With approval from Maco Cortes, You are permitted to use the program for educational purposes only.
- * 
- * MODIFICATION AND REDISTRIBUTION 
- * Unless with written approval obtained from Maco Cortes, 
- * You are NOT allowed to modify, copy, redistribute, and sell the Software.
- *
- * For any concerns, you may reach Maco Cortes via:
- * maco.techdepot@gmail.com
- * facebook.com/Maaacoooo
- * maco@techdepot-ph.com
- * TechDepot-PH.com
- */
-?>
     <!-- Favicons-->
     <link rel="icon" href="<?=base_url('assets/images/favicon/sti_45x45.png')?>" sizes="45x45">
 
--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,27 +1,3 @@
-<?php
-/*
- * Copyright (c)2017, Jenmar "Maco" Cortes
- * Copyright TechDepot PH
- * All Rights Reserved
- * 
- * This license is a legal agreement between you and the Maco Cortes
- * for the use of STI Online Voting Systen referred to as the "Software"
- * By obtaining the Software you agree to comply with the terms and conditions of this license.
- *
- * PERMITTED USE
- * With approval from Maco Cortes, You are permitted to use the program for educational purposes only.
- * 
- * MODIFICATION AND REDISTRIBUTION 
- * Unless with written approval obtained from Maco Cortes, 
- * You are NOT allowed to modify, copy, redistribute, and sell the Software.
- *
- * For any concerns, you may reach Maco Cortes via:
- * maco.techdepot@gmail.com
- * facebook.com/Maaacoooo
- * maco@techdepot-ph.com
- * TechDepot-PH.com
- */
-?>
 <!-- START FOOTER -->
   <footer class="page-footer footer indigo darken-3">
     <div class="footer-copyright">
